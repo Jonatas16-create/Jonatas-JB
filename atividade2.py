85d1e57c1d15c788c21a2f5aab9a6be1479bbf79
Jonatas16-create <jonatas.bevenuto@academico.ifpb.edu.br>
2025-07-04 14:44:04 +0000

    criei

--- atividade2.py
+++ atividade2.py
@@ -1,1 +1,11 @@
-print ("A vida é incrivel")
+print ("numeros pares:")
+par = 2
+for i in range(20):
+    print(par)
+    par += 2
+
+    print ("numeros impar:")
+    impar = 1
+    for i in range(20):
+        print(impar)
+        impar += 1
